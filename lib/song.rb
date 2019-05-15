af20169d39d29573daae60851fe13dd9623d7fdb
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-15 02:53:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,22 @@
 class Song 
 attr_accessor :name, :artist, :genre
 @@count=0
+@@genres= []
 
 def initialize(name, artist, genre)
   @name 
   @artist
   @genre
   @@count +=1
+  @@genres << genre
 end
+
+def count
+  @@count
+end
+
+def genres
+  @@genres
+end 
 
 end
